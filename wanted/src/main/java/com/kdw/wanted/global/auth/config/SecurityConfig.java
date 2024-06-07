c6d74dcbd6e97457a9c877b6d39af4c79e4b85d9
kdw3238 <kdw3238@naver.com>
2024-06-07 22:56:30 +0900

    [feat] Spring security, JWT 로직
    security config 설정 및 JwtToken 발행 서비스 구현

---
+++ wanted/src/main/java/com/kdw/wanted/global/auth/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package com.kdw.wanted.global.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.kdw.wanted.global.auth.filter.JwtAuthenticationFilter;
+import com.kdw.wanted.global.auth.service.JwtTokenProvider;
+
+import lombok.AllArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+
+	private final JwtTokenProvider jwtTokenProvider;
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.httpBasic(
+				httpBasic->
+					httpBasic.disable())
+			.csrf(
+				csrfConfig->
+					csrfConfig.disable()
+					)
+			.sessionManagement(
+				sessionManage->
+					sessionManage.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.headers(
+					headerConifg->
+						headerConifg
+							.frameOptions(frameOptionsConfig->frameOptionsConfig.disable())
+					)
+			.authorizeHttpRequests(
+					authorizeRequests -> 
+						authorizeRequests
+							.requestMatchers("/user/signin", "/user/signup").permitAll()
+							.requestMatchers(HttpMethod.GET, "/products").permitAll()
+							.requestMatchers("/err").permitAll()
+							.anyRequest().permitAll()
+					)
+			.addFilterBefore(
+					new JwtAuthenticationFilter(jwtTokenProvider), 
+					UsernamePasswordAuthenticationFilter.class);
+			
+		
+		
+		return http.build();
+	}
+}
