971c19c96afa149687b03b17d240610da7464ae4
kdw3238 <kdw3238@naver.com>
2024-06-11 17:03:31 +0900

    [feat] ProductTransactionRequestDto Approve 추가
    판매 승인을 위한 request dto 추가

--- wanted/src/main/java/com/kdw/wanted/domain/product/dto/request/ProductTransactionRequestDto.java
+++ wanted/src/main/java/com/kdw/wanted/domain/product/dto/request/ProductTransactionRequestDto.java
@@ -1,12 +1,5 @@
 package com.kdw.wanted.domain.product.dto.request;
 
-import java.util.UUID;
-
-import com.kdw.wanted.domain.account.domain.Account;
-import com.kdw.wanted.domain.product.domain.Product;
-import com.kdw.wanted.domain.product.domain.ProductTransaction;
-
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -14,15 +7,14 @@ public class ProductTransactionRequestDto {
 	
 	@Data
 	public static class Make{
-		
 		@NotNull
 		Long productId;
 		
-		public ProductTransaction toEntity() { 
-			return ProductTransaction.builder()
-					.product(Product.builder().id(productId).build())
-					.build();
-		}
-		
+	}
+	
+	@Data
+	public static class Approve{
+		@NotNull
+		Long productId;
 	}
 }
