0f44780c7f44999aee37794d233f4d0e1df4f654
kdw3238 <kdw3238@naver.com>
2024-06-14 19:56:42 +0900

    [fix] Auth exception
    jwttoken이 없는 경우의 예외상황 추가

--- wanted/src/main/java/com/kdw/wanted/global/auth/filter/JwtAuthenticationEntryPoint.java
+++ wanted/src/main/java/com/kdw/wanted/global/auth/filter/JwtAuthenticationEntryPoint.java
@@ -1,12 +1,17 @@
 package com.kdw.wanted.global.auth.filter;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import com.kdw.wanted.global.error.ErrorCode;
+import com.kdw.wanted.global.error.exception.AuthException;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -24,7 +29,15 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException, ServletException {
-		resolver.resolveException(request, response, null, (Exception)request.getAttribute("exception"));
+		if(authException instanceof InsufficientAuthenticationException) {
+			resolver.resolveException(request, response, null, new AuthException(ErrorCode.MISSING_AUTHENTICATION));
+		}
+		else {
+			resolver.resolveException(request, response, null,
+					Optional.ofNullable((Exception)request.getAttribute("exception"))
+						.orElse(authException)
+					);
+		}
 	}
 	
 	
