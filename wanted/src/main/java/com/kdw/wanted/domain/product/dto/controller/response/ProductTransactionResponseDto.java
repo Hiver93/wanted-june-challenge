a5faaa45565a05b8d9c21e1b87650c31e08b17f6
kdw3238 <kdw3238@naver.com>
2024-06-27 18:14:48 +0900

    [fix] ProductTransactionResponseDto 수정
    Service dto 추가로 인한 controller dto 수정

--- wanted/src/main/java/com/kdw/wanted/domain/product/dto/controller/response/ProductTransactionResponseDto.java
+++ wanted/src/main/java/com/kdw/wanted/domain/product/dto/controller/response/ProductTransactionResponseDto.java
@@ -1,10 +1,11 @@
 package com.kdw.wanted.domain.product.dto.controller.response;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 
-import com.kdw.wanted.domain.product.domain.Product;
 import com.kdw.wanted.domain.product.domain.ProductTransaction;
-import com.kdw.wanted.domain.product.domain.enums.ProductTransactionState;
+import com.kdw.wanted.domain.product.dto.service.response.ProductTransactionServiceResponse;
 
 import lombok.Builder;
 import lombok.Data;
@@ -13,26 +14,57 @@ public class ProductTransactionResponseDto {
 	
 	@Data
 	@Builder
-	public static class Element{
-		Long id;
+	public static class Transactions{
 		
-		Long price;
+		List<Element> complete;
 		
-		String state;
+		List<Element> inProgress;
+		
+		public static Transactions fromServiceDto(ProductTransactionServiceResponse.Transactions transactions) {
+			return Transactions.builder()
+									.complete(transactions.getComplete().stream()
+														.map(Element::fromEntity)
+														.toList())																			
+									.inProgress(transactions.getInProgress().stream()
+														.map(Element::fromEntity)
+														.toList())
+									.build();
+									
+		}
+		
+		@Data
+		@Builder
+		public static class Element{
+			Long id;
+			
+			Long price;
+			
+			String state;
 
-		LocalDateTime createdAt;
-		
-		LocalDateTime updatedAt;
-		
-		Product product;
-		public static ProductTransactionResponseDto.Element fromEntity(ProductTransaction productTransaction) {
-			return builder()
-					.id(productTransaction.getId())
-					.price(productTransaction.getPrice())
-					.state(productTransaction.getState().getDesc())
-					.createdAt(productTransaction.getCreatedAt())
-					.updatedAt(productTransaction.getUpdatedAt())
-					.build();
+			LocalDateTime createdAt;
+			
+			LocalDateTime updatedAt;
+			
+			Long productId;
+			
+			String productName;
+			
+			UUID providerId;
+			
+			String providerName;
+			public static Element fromEntity(ProductTransaction productTransaction) {
+				return builder()
+						.id(productTransaction.getId())
+						.price(productTransaction.getPrice())
+						.state(productTransaction.getState().getDesc())
+						.productId(productTransaction.getProduct().getId())
+						.productName(productTransaction.getProduct().getName())
+						.providerId(productTransaction.getProduct().getAccount().getId())
+						.providerName(productTransaction.getProduct().getAccount().getUsername())
+						.createdAt(productTransaction.getCreatedAt())
+						.updatedAt(productTransaction.getUpdatedAt())
+						.build();
+			}
 		}
 	}
 }
